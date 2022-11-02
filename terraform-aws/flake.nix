074732afdf16acb6ca3a8421c90884aec259e829
Zhong Jianxin <azuwis@gmail.com>
2022-11-02 10:58:23 +0800

    terraform-aws: Use numtide/devshell

--- terraform-aws/flake.nix
+++ terraform-aws/flake.nix
@@ -1,13 +1,17 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.devshell.url = "github:numtide/devshell";
+  inputs.devshell.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.devshell.inputs.flake-utils.follows = "flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, devshell }:
   flake-utils.lib.eachDefaultSystem (system: let
     pkgs = nixpkgs.legacyPackages.${system};
+    mkShell = devshell.legacyPackages.${system}.mkShell;
   in {
-    devShell = pkgs.mkShell {
-      nativeBuildInputs = with pkgs; [
+    devShell = mkShell {
+      packages = with pkgs; [
         (terraform_1_0.withPlugins (provider: [
           provider.aws
         ]))
