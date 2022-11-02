be9c819ca069ac65df3078f884e9ae5a92dc88b9
Zhong Jianxin <azuwis@gmail.com>
2022-11-02 10:55:35 +0800

    ansible-2.12: Use numtide/devshell

--- ansible-2.12/flake.nix
+++ ansible-2.12/flake.nix
@@ -1,14 +1,18 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.devshell.url = "github:numtide/devshell";
+  inputs.devshell.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.devshell.inputs.flake-utils.follows = "flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, devshell }:
   flake-utils.lib.eachDefaultSystem (system:
   let
     pkgs = nixpkgs.legacyPackages.${system};
+    mkShell = devshell.legacyPackages.${system}.mkShell;
   in {
-    devShell = pkgs.mkShell {
-      nativeBuildInputs = [
+    devShell = mkShell {
+      packages = [
         # pkgs.ansible
         (pkgs.python3.withPackages(ps: [
           ps.ansible-core
